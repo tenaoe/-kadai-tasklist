e4d8706e5e7e0e8aee19659b94010da10bff4454
ten.aoe <bubble1000ta@gmail.com>
2019-05-02 22:41:46 +0900

    Modify Index/NewServlet

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -2,18 +2,15 @@ package controllers;
 
 import java.io.IOException;
 
-import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-import java.sql.Timestamp;
-import javax.persistence.EntityManager;
 import models.Tasklist;
-import utils.DBUtil;
+
 
 /**
  * Servlet implementation class NewServlet
@@ -34,27 +31,17 @@ public class NewServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager em = DBUtil.createEntityManager();
-        em.getTransaction().begin();
+        request.setAttribute("_token", request.getSession().getId());
 
-        Tasklist m = new Tasklist();
+        request.setAttribute("tasklist", new Tasklist());
 
-        String title = "ten";
-        m.setTitle(title);
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasklists/new.jsp");
+        rd.forward(request, response);
 
-        String content = "hello";
-        m.setContent(content);
-
-        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-        m.setCreated_at(currentTime);
-        m.setUpdated_at(currentTime);
-
-        em.persist(m);
-        em.getTransaction().commit();
-
-        response.getWriter().append(Integer.valueOf(m.getId()).toString());
-
-        em.close();
     }
-
 }
+
+
+
+
+
